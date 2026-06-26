d8e23a8654f4bb56a6f4b60eb6ae52c50775aeb4
WBArick <wahibarick@gmail.com>
2026-06-26 12:16:24 +0600

    Add files via upload

---
+++ class_work2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define PI 3.1416
+int main()
+{
+    double radius, area;
+    printf("Enter the radius of the circle:");
+    scanf("%lf",&radius);
+    printf("Area of the circle = %.2f", area=PI*radius*radius);
+    return 0;
+}
