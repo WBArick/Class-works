c917457a2703f631a93b350a2e86fddc7076ba55
WBArick <wahibarick@gmail.com>
2026-07-22 20:12:32 +0600

    Add files via upload

---
+++ triangle_while.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+    int i=1;
+    while(i<=5)
+    {
+        int j=1;
+        while(j<=i)
+        {
+            printf("*");
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+    return 0;
+}
