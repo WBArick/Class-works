97c6b15ffc991c71889cc50369dc1c62789f3fe8
WBArick <wahibarick@gmail.com>
2026-07-27 21:09:46 +0600

    Add files via upload

---
+++ alphabet_triangle2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{
+    int N=5;
+    int i;
+    int j;
+    char ch ='A';
+    for(i=1; i<=N; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            printf("%c", ch);
+            
+        }
+        ch++;
+        printf("\n");
+    }
+    return 0;
+}
