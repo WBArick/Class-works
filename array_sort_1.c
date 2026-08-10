ebbbe0da2f37879753909e18564a395e4bfb3c18
WBArick <wahibarick@gmail.com>
2026-08-10 22:40:17 +0600

    Add files via upload

---
+++ array_sort_1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(void)
+{
+    int size;
+    int arr[50];
+    int i;
+    int j;
+    int temp;
+    printf("How many numbers do you have?");
+    scanf("%d", &size);
+    printf("Enter the numbers:");
+    for(i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for(i = 0; i < size - 1; i++)
+    {
+        for(j = 0; j < size - 1 - i; j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    printf("Sorted numbers are: ");
+    for(i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}            
